271e45d845c1cdbdf58434f7569bf0fdfeef8cbc
zafar.takhirov <ztakhiro@irnbru.adsiv.analog.com>
2014-09-29 16:17:05 -0700

    AUTOCOMMIT(ztakhiro@irnbru.adsiv.analog.com): Mon Sep 29 16:17:05 PDT 2014

--- cube.cpp
+++ cube.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
+// Test colors first:
+void printColor(string str) {
+  system("Color 1A");
+  cout << str;
+}
+
+int main() {
+  printColor("TEST\n");
+}
